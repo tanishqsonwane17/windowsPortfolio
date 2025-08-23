9af7e6847c946470cc9a63fc766d8432692eab76
Tanishq Sonwane <tanishqsonwane32@gmail.com>
2025-08-23 09:45:07 +0530

    folder creating

--- src/components/RightMenu.jsx
+++ src/components/RightMenu.jsx
@@ -1,35 +1,47 @@
 import React from 'react'
-import { CiBoxList } from "react-icons/ci";
-import { BsArrowDownUp } from "react-icons/bs";
+import { CiBoxList, CiCirclePlus } from "react-icons/ci";
+import { BsArrowDownUp, BsPen } from "react-icons/bs";
 import { IoRefresh } from "react-icons/io5";
 import { LuUndo } from "react-icons/lu";
-import { CiCirclePlus } from "react-icons/ci";
-import { BsPen } from "react-icons/bs";
 import { TbTerminal2 } from "react-icons/tb";
-const RightMenu = () => {
+
+const RightMenu = ({ onNewFolder }) => {
   return (
-    <>
-    <div className=' w-50 backdrop-blur-2xl bg-[#d5cece63] rounded-2xl'>
-      <ul className='flex flex-col gap-4 px-4 py-4'>
-       <div className='flex flex-col gap-2'>
-         <li className='flex gap-2 items-center'> <CiBoxList className='inline-block'/> View</li>
-        <li className='flex gap-2 items-center' > <BsArrowDownUp className='inline-block'/> Short by</li>
-        <li  className='flex gap-2 items-center'> <IoRefresh className='inline-block'/> refresh</li>
-       </div>
-        <hr className='text-[#00000047]'/>
-       <div className='flex flex-col gap-2'>
-       <li
-        className='flex gap-2 items-center'> <CiCirclePlus className='inline-block'/> new</li>
-         <li className='flex gap-2 items-center'> <LuUndo className='inline-block'/>  undo</li>
-       </div>
+    <div className='w-50 backdrop-blur-2xl bg-[#d5cece63] rounded-2xl'>
+      <ul className='flex flex-col gap-4 px-2 py-4'>
+        <div className='flex flex-col gap-2'>
+          <li className='flex gap-2 items-center cursor-pointer hover:bg-[#d5cece63] px-4 rounded-2xl'> 
+            <CiBoxList /> View
+          </li>
+          <li className='flex gap-2 items-center cursor-pointer hover:bg-[#d5cece63] px-4 rounded-2xl'> 
+            <BsArrowDownUp /> Sort by
+          </li>
+          <li className='flex gap-2 items-center cursor-pointer hover:bg-[#d5cece63] px-4 rounded-2xl'> 
+            <IoRefresh /> Refresh
+          </li>
+        </div>
         <hr className='text-[#00000047]' />
-       <div className='flex flex-col gap-2'>
-         <li className='flex gap-2 items-center'> <BsPen className='inline-block'/> personalize</li>
-        <li className='flex gap-2 items-center'> <TbTerminal2 className='inline-block'/> open in terminal</li>
-       </div>
+        <div className='flex flex-col gap-2'>
+          <li
+            onClick={onNewFolder}
+            className='flex gap-2 items-center cursor-pointer hover:bg-[#d5cece63] px-4 rounded-2xl'>
+            <CiCirclePlus /> New Folder
+          </li>
+          <li className='flex gap-2 items-center cursor-pointer hover:bg-[#d5cece28] px-4 rounded-2xl'> 
+            <LuUndo /> Undo
+          </li>
+        </div>
+        <hr className='text-[#00000047]' />
+        <div className='flex flex-col gap-2'>
+          <li className='flex gap-2 items-center cursor-pointer hover:bg-[#d5cece63] px-4 rounded-2xl'> 
+            <BsPen /> Personalize
+          </li>
+          <li className='flex gap-2 items-center cursor-pointer hover:bg-[#d5cece63] px-4 rounded-2xl'> 
+            <TbTerminal2 /> Open in terminal
+          </li>
+        </div>
       </ul>
     </div>
-    </>
   )
 }
 
--- src/screens/Home.jsx
+++ src/screens/Home.jsx
@@ -1,8 +1,9 @@
-import React, { useState, useRef, } from "react";
+import React, { useState, useRef } from "react";
 import RightMenu from "../components/RightMenu";
 
 const Home = () => {
   const [menu, setMenu] = useState({ visible: false, x: 0, y: 0 });
+  const [folders, setFolders] = useState([]);
   const menuRef = useRef(null);
 
   const handleRightClick = (e) => {
@@ -21,17 +22,19 @@ const Home = () => {
       y = window.innerHeight - menuHeight - 10;
     }
 
-    setMenu({
-      visible: true,
-      x,
-      y,
-    });
+    setMenu({ visible: true, x, y });
   };
 
   const handleClick = () => {
     if (menu.visible) {
       setMenu({ ...menu, visible: false });
     }
+  };
+
+  // ✅ New Folder add karne ka handler
+  const handleNewFolder = () => {
+    setFolders([...folders, { id: Date.now(), name: `New Folder ${folders.length + 1}` }]);
+    setMenu({ ...menu, visible: false }); // menu close after click
   };
 
   return (
@@ -41,15 +44,21 @@ const Home = () => {
       className="h-screen w-full bg-slate-400 bg-[url('https://4kwallpapers.com/images/wallpapers/windows-11-stock-3d-5689x2400-10781.png')] bg-cover bg-center relative"
     >
       <div className="flex flex-col gap-5 flex-wrap w-0 h-full p-4">
-        <div className="h-12 w-12 bg-yellow-400 rounded-2xl"></div>
+        {/* Render Folders */}
+        {folders.map((folder) => (
+          <div key={folder.id} className="h-12 w-12 bg-yellow-400 rounded-2xl flex items-center justify-center text-xs text-black">
+            📁
+          </div>
+        ))}
       </div>
 
       {menu.visible && (
         <div
           ref={menuRef}
           style={{ top: menu.y, left: menu.x }}
-          className="absolute z-50">
-          <RightMenu />
+          className="absolute z-50"
+        >
+          <RightMenu onNewFolder={handleNewFolder} />
         </div>
       )}
     </div>
